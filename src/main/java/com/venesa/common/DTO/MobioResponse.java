c95651b87cfeaa17ae6ac1b1128bbbe8a7fb0e4a
Thuan Le Minh (HOHN-IT) <thuanlm@deaura.com.vn>
2020-08-11 17:44:08 +0700

    config environment

--- src/main/java/com/venesa/common/DTO/MobioResponse.java
+++ src/main/java/com/venesa/common/DTO/MobioResponse.java
@@ -1,5 +1,7 @@
 package com.venesa.common.DTO;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,9 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class MobioResponse implements Serializable {
     private String code;
     private String message;
-
+    private Object data;
 }
