8e5b47667776af5ea30229b8ed7f2b64263834d9
Thuan Le Minh (HOHN-IT) <thuanlm@deaura.com.vn>
2020-08-14 16:39:14 +0700

    add feature webclient

--- src/main/java/com/venesa/common/CommonApplication.java
+++ src/main/java/com/venesa/common/CommonApplication.java
@@ -2,12 +2,17 @@ package com.venesa.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class CommonApplication {
       public static void main(String[] args) {
         SpringApplication.run(CommonApplication.class, args);
     }
-
+    @Bean
+    public WebClient getWebclient() {
+        return WebClient.create();
+    }
 
 }
---
+++ src/main/java/com/venesa/common/DTO/ResponseData.java
@@ -0,0 +1,17 @@
+package com.venesa.common.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseData<T>{
+	private int errorCode;
+	private String errorMessage;
+	private T data;
+
+}
